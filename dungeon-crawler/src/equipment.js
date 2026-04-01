eb12f9ee3f9093c71454ef95a885ebebdee1ee7f
rob croft <rob@robincroft.com>
2026-04-01 19:33:34 +0100

    increase range attack range

--- dungeon-crawler/src/equipment.js
+++ dungeon-crawler/src/equipment.js
@@ -2977,7 +2977,7 @@ export function useHand(memberIndex, hand, silent = false) {
     }
   }
 
-  const maxRange = isRanged ? 4 : 1;
+  const maxRange = isRanged ? 8 : 1;
 
   // Find the alive monster closest to the player that is directly in front
   const target = isBuff ? null : _closestMonsterInFront(maxRange);
