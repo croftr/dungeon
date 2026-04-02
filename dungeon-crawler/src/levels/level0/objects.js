2e9ff0a6a6cd9e394a0900d5d00fef78ea304c70
rob croft <rob@robincroft.com>
2026-04-02 13:21:33 +0100

    party panel style

--- dungeon-crawler/src/levels/level0/objects.js
+++ dungeon-crawler/src/levels/level0/objects.js
@@ -29,7 +29,7 @@ export function spawnLevel0Objects(ctx) {
     addChest(group, loader, 13.4, 10, Math.PI / 2, -0.1, [
         { name: 'Gold Coins', quantity: 100 },
         'Torch', 'Potion of Invincibility', 'Potion of Unseen', "Life Essence", "Iron Ore", "Life Essence",
-        "Iron Ore", "Iron Ore", "Red Crystal", "Blue Crystal", "Bronze Key", "Bone Key",
+        "Iron Ore", "Iron Ore", "Scroll of Fireball", "Scroll of Cure Poison", "Scroll of Rejuvenate",
         "Potions Parchment", "Forge Armour Parchment", "Forge Weapons Parchment", "Minor Potions Parchment", "Party Potions Parchment", "Scroll of Incinerate"
     ], asset('/items/stash.glb'), true, 0, 'Stash');
 
